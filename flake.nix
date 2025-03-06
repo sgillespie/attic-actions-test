1c6d8d667eda545e1eab24ca618c6f45fa162e67
Sean D Gillespie <sean@mistersg.net>
2025-03-06 14:29:45 -0500

    build: Update build target to force a build

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in {
-        packages.default = pkgs.pkgsCross.musl64.hello-cpp;
+        packages.default = pkgs.pkgsCross.aarch64-multiplatform-musl.hello-cpp;
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [ attic-client attic-server ];
